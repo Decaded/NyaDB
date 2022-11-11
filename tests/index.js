a71f9730ef70323d4e4a07e2c692f2f41e244e78
Decaded <decadedo@gmail.com>
2022-11-12 00:16:47 +0100

    close tests after they are done

--- tests/index.js
+++ tests/index.js
@@ -1,27 +1,27 @@
-const NyaDB = require("../index");
+const NyaDB = require('../index');
 const nyadb = new NyaDB();
 const mockDatabase = {
-	yellow: ["banana", "citrus"],
-	red: ["apple", "paprika"],
+	yellow: ['banana', 'citrus'],
+	red: ['apple', 'paprika'],
 };
 
 // Create a database called "numbers"
-nyadb.createDatabase("numbers");
+nyadb.createDatabase('numbers');
 // insert increasing numbers into the database, one by one
 for (let i = 0; i < 10; i++) {
-	nyadb.setDatabase("numbers", {
+	nyadb.setDatabase('numbers', {
 		[i]: i,
 	});
 }
 
 // Create a database called "fruits"
-nyadb.createDatabase("fruits");
+nyadb.createDatabase('fruits');
 
 // Insert mock data into the fruits database
-nyadb.setDatabase("fruits", mockDatabase);
+nyadb.setDatabase('fruits', mockDatabase);
 
 // Create a database called "deleteMe"
-nyadb.createDatabase("deleteMe");
+nyadb.createDatabase('deleteMe');
 
 setTimeout(() => {
 	console.log(nyadb.database);
@@ -29,7 +29,7 @@ setTimeout(() => {
 
 // Log the contents of the database "numbers" to the console after 6 seconds
 setTimeout(() => {
-	console.log(nyadb.getDatabase("numbers"));
+	console.log(nyadb.getDatabase('numbers'));
 }, 6000);
 
 // Log all the databases to the console after 8 seconds
@@ -39,6 +39,12 @@ setTimeout(() => {
 
 // Delete the database "deleteMe" after 10 seconds, then log all the databases to the console again
 setTimeout(() => {
-	nyadb.deleteDatabase("deleteMe");
+	nyadb.deleteDatabase('deleteMe');
 	console.log(nyadb.getDatabaseList());
 }, 10000);
+
+// Close tests
+setTimeout(() => {
+	console.log('Done');
+	process.exit();
+}, 12000);
