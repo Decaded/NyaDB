789a65e41c16082c0a490add52c1620b7919041c
Decaded <12958507+Decaded@users.noreply.github.com>
2023-03-07 15:34:40 +0100

    modified:   index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -6,4 +6,10 @@ declare class NyaDB {
     setDatabase(name: any, data: any): void;
     getDatabase(name: any): any;
     getDatabaseList(): string[];
+
+    create(name: any): void;
+    delete(name: any): void;
+    set(name: any, data: any): void;
+    get(name: any): any;
+    getList(): string[];
 }
