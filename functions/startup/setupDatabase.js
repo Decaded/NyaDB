925d7aa707d7906aab2b1da261fd9850398c7fa1
Decaded <me@decaded.dev>
2024-02-19 09:51:22 +0100

    Add error handling

--- functions/startup/setupDatabase.js
+++ functions/startup/setupDatabase.js
@@ -1,20 +1,18 @@
 const { existsSync, mkdirSync, writeFileSync } = require('fs');
 const config = require('../../config/config');
 
-/**
- * Creates a file if it doesn't exist, initializing it with the provided content.
- * It can be customized in 'config/default.json' file.
- * @param {string} databaseFolderPath - The path of the main database folder.
- * @param {string} filePath - The path of the database file to create.
- * @param {object} content - The content to initialize the file with.
- * @returns {boolean} - True if the file was created successfully, false otherwise.
- */
+const databaseFolderPath = `./${config.storage.databaseFolderName}`;
+const databaseFilePath = `/${config.storage.databaseFileName}`;
 
-// Create database folder if it doesn't exist
-if (!existsSync(config.databaseFolderPath)) {
-	mkdirSync(config.databaseFolderPath);
-}
-// Create database file if it doesn't exist
-if (!existsSync(config.databaseFolderPath + config.filePath)) {
-	writeFileSync(config.databaseFolderPath + config.filePath, config.content);
+try {
+	// Create database folder if it doesn't exist
+	if (!existsSync(databaseFolderPath)) {
+		mkdirSync(databaseFolderPath);
+	}
+	// Create database file if it doesn't exist
+	if (!existsSync(databaseFolderPath + databaseFilePath)) {
+		writeFileSync(databaseFolderPath + databaseFilePath, config.storage.content);
+	}
+} catch (error) {
+	console.error('NyaDB: Error setting up database:', error);
 }
