b34a37d4db78c74f2e07f435aea1bfb4246bd6e9
Decaded <me@decaded.dev>
2024-02-19 09:43:56 +0100

    Update TypeScript declaration

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,3 @@
-export default NyaDB;
-
 /**
  * Main NyaDB class that handles all database operations.
  */
@@ -7,32 +5,37 @@ declare class NyaDB {
     /**
      * Creates a new database with the given name, if it doesn't already exist.
      * @param {string} name - The name of the database to create.
+     * @returns {boolean} - Whether or not the database was created successfully.
      */
-    create(name: string): void;
+    create(name: string): boolean;
 
     /**
      * Deletes the database with the provided name, if it exists.
      * @param {string} name - The name of the database to delete.
+     * @returns {boolean} - Whether or not the database was deleted successfully.
      */
-    delete(name: string): void;
+    delete(name: string): boolean;
 
     /**
      * Sets the database with the given name to the provided JSON object.
      * @param {string} name - The name of the database to set.
      * @param {object} data - The JSON object to set the database to.
+     * @returns {boolean} - Whether or not the database was set successfully.
      */
-    set(name: string, data: object): void;
+    set(name: string, data: object): boolean;
 
     /**
      * Returns the database object for the provided name, or false if it doesn't exist.
      * @param {string} name - The name of the database to retrieve.
-     * @returns {object|false} The database object, or false if not found.
+     * @returns {object|false} - The database object, or false if not found.
      */
     get(name: string): object | false;
 
     /**
      * Returns an array of all database names.
-     * @returns {string[]} An array containing the names of all databases.
+     * @returns {string[]} - An array containing the names of all databases.
      */
     getList(): string[];
 }
+
+export default NyaDB;
